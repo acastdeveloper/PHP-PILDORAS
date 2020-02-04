9ffa28230690856d6d1900beb14ae7ab8f70649b
Àlex Castelló <ac.desenvolupador@gmail.com>
2020-02-04 22:07:28 +0100

    Video 16. L'Operador Ternari

---
+++ 16/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>16. L'Operador Ternari</title>
+</head>
+<body>
+  <h1>16. Operador Ternari</h1>
+
+  <pre>
+    Condició? Valor si és TRUE : Valor si és FALSE.
+  </pre>
+
+<p><strong>Exemple:</strong></p>
+<pre>
+    $edad=3;
+    echo $edad &lt; 18 ? "Ets menor d'edat." : "Ets adult.";
+    // "Ets menor d'edat"
+</pre>
+
+</body>
+</html>
